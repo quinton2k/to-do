993e5b46d6a1571f3ed2802127499f3adc86b3cd
Kim Hong Minh Quan <kimhongminhquan@gmail.com>
2022-07-02 14:25:57 +0700

    import matcard, matform, maticon basic build input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ToDoComponent } from './to-do/to-do.component'
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ToDoComponent } from './to-do/to-do.component'
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,10 @@ import { ToDoComponent } from './to-do/to-do.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
